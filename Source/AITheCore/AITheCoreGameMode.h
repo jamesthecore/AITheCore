332acd7d34937712a0816fc5ad70e0006d63b8e8
James Phelan <jwdeharopwd@gmail.com>
2024-02-14 16:53:04 +0100

    Commit with the Unreal Engine project.

---
+++ Source/AITheCore/AITheCoreGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AITheCoreGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AAITheCoreGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AAITheCoreGameMode();
+};
+
+
+
